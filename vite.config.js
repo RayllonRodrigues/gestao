3f60c14f0270839b38df974f4afb9632a86d49e1
Rayllon Rodrigues <rayllonrodrigue@gmail.com>
2025-09-30 15:42:14 -0300

    Atualizando

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
 export default defineConfig({
-  base: "./",   // ESSENCIAL p/ Netlify
-  plugins: [react()],
+    base: "./", // ESSENCIAL p/ Netlify
+    plugins: [react()],
 });
